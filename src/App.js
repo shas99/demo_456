48b267f6ba61a8eea209ce7758aecc1ed0648217
shas99 <pshasvathan1999@gmail.com>
2022-03-29 14:43:27 +0530

    Init commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1> Hello World ! </h1>
+    <h3>Hello Chathushka </h3> 
+    <h10> by Chathushka</h10> 
+    <br></br>
+    <h1> Hello World ! </h1>
+    <h3>Hello Chamath </h3> 
+    <h10> by Chamath</h10>
+    </div>
+  );
+}
+
+export default App;
